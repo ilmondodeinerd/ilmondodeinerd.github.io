fbdece126a50017ac098df410a608e9aae34b6dd
ilmondodeinerd <sergionoto23@gmail.com>
2025-04-17 02:37:06 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@100..900&display=swap');     
+        @font-face {
+        font-family: CCComicrazy Roman;
+        src: url(CCComicrazy\ Roman.ttf);
+      }
+        </style>
+        
+        
+        <title>Registrazione</title>
+        <link href="corpo.css" rel="stylesheet">
+    </head>
+    <html>
+        <form action="registrazione.php" method="post">
+        <div class="imga">
+            <img src="logo.png">
+        </div>
+        <div class="imga1">
+            <img src="registrazione.png">
+        </div>
+        <div class="login">
+            <input type="text" name="username" id="username" placeholder="Inserisci il tuo nome">
+            <input type="email" name="email" id="email" placeholder="Inserisci la tua email">
+            <input type="password" name="password" id="password" placeholder="Inserisci la tua password"><br><br>
+           <button class="pulsante" type="submit" name="invia">AVANTI</button><br><br>
+           <div class="account">
+           HAI GIA' UN ACCOUNT <a href="login.html">ACCEDI</a>
+           </div>
+         </div>
+        <div class="riservatezza">
+            2025 Il Mondo dei Nerd. Tutti i diritti riservati.
+        </div>
+        </form>
+        </html>
